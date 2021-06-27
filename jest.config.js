43d1d2ea217a0980d7275b3a8c6cbead0a11f2ff
Lucas <lucas.zrc@gmail.com>
2021-06-27 04:34:28 -0300

    Added lint, prettie and some setup for jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { resolve } = require('path')
+
+// Dir raiz da aplicacao
+const root = resolve(__dirname);
+
+module.exports = {
+    rootDir: root,
+    displayName: 'root-tests',
+    testMatch: ['<rootDir>/src/**/*.test.ts'],
+    testEnvironment: 'node',
+    clearMocks: true,
+    preset: 'ts-jest',
+    moduleNameMapper: { 
+        '@src/(.*)': '<rootDir>/src/$1',
+        '@test/(.*)': '<rootDir>/test/$1'
+    },
+
+};
